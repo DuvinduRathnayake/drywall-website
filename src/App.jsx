ae3640385ff7665ae82092c849445c57cb9d491c
Duvindu Rathnayake <duvindurathnayake77@gmail.com>
2025-08-13 19:47:58 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='min-h-screen bg-slate-100 text-slate-900'>
+        <h1>Hello React</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/app/router.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
+import Projects from '../pages/Projects';
+import Services from '../pages/Services';
+
+export const router = createBrowserRouter([{}]);
